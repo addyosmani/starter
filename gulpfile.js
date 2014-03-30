350dbd7021667b1e01bc1701d3873ff388b5dd83
Addy Osmani <addyosmani@gmail.com>
2014-03-30 16:46:52 +0100

    Add starting point.

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+
+
+// Scripts
+gulp.task('scripts', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint('.jshintrc'))
+        .pipe($.jshint.reporter('default'))
+        .pipe($.size());
+});
+
+// HTML
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+      .pipe($.useref())
+      .pipe(gulp.dest('dist'))
+      .pipe($.size());
+});
+
+// Images
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe($.size());
+});
+
+// Clean
+gulp.task('clean', function () {
+    return gulp.src(['dist/scripts', 'dist/images'], {read: false}).pipe($.clean());
+});
+
+// Bundle
+gulp.task('bundle', ['scripts'], $.bundle('./app/*.html'));
+
+// Build
+gulp.task('build', ['html', 'bundle', 'images']);
+
+// Default task
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+// Connect
+gulp.task('connect', $.connect.server({
+    root: ['app'],
+    port: 9000,
+    livereload: true
+}));
+
+// Inject Bower components
+gulp.task('wiredep', function () {
+    gulp.src('app/styles/*.scss')
+        .pipe(wiredep({
+            directory: 'app/bower_components',
+            ignorePath: 'app/bower_components/'
+        }))
+        .pipe(gulp.dest('app/styles'));
+
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'app/bower_components',
+            ignorePath: 'app/'
+        }))
+        .pipe(gulp.dest('app'));
+});
+
+// Watch
+gulp.task('watch', ['connect'], function () {
+    // Watch for changes in `app` folder
+    gulp.watch([
+        'app/*.html',
+        
+        'app/scripts/**/*.js',
+        'app/images/**/*'
+    ], function(event) {
+        return gulp.src(event.path)
+            .pipe($.connect.reload());
+    });
+    
+
+    // Watch .js files
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+
+    // Watch image files
+    gulp.watch('app/images/**/*', ['images']);
+
+    // Watch bower files
+    gulp.watch('app/bower_components/*', ['wiredep']);
+});
